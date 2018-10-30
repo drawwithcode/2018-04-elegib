2f6a0804088d92f5b2db3542b9835068763c11f3
elegib <elena.gibby97@gmail.com>
2018-10-30 12:32:33 +0100

    object

--- sketch.js
+++ sketch.js
@@ -1,255 +1,150 @@
-var myData;
-var myImage;
-var sposta = 0;
-var match = 0;
-var click = 0;
-var button = 0;
-var cambiaPagina = 0;
-var imInterstellar;
-var imFightclub;
-var imMatrix;
-var imPsycho;
-var imDeparted;
-var imPulpfiction;
-var imInseption;
-var imPianist;
-var imForrestgump;
-var imGladietor;
-var imMemento;
-var imSchindler;
-var arrowHover;
-var arrow;
+function preload(){
+};
 
+var figureS = [];
+var figureD = [];
+var t = 0;
+var tempo = 3000;
 
-
-function preload() {
-  myData = loadJSON('assets/top-rated-movies-01.json');
-  imInterstellar = loadImage ('./assets/interstellar.jpg ')
-  imFightclub = loadImage ('./assets/fightclub.jpg ')
-  imMatrix = loadImage ('./assets/matrix.jpg ')
-    imPsycho = loadImage ('./assets/psycho.jpg ')
-      imDeparted = loadImage ('./assets/departed.jpg ')
-    imPulpfiction = loadImage ('./assets/pulpfiction.jpg ')
-            imInception = loadImage ('./assets/inception.jpg ')
-            imPianist = loadImage ('./assets/pianist.jpg ')
-            imForrestgump = loadImage ('./assets/forrestgump.jpg ')
-              imGladietor = loadImage ('./assets/gladietor.jpg ')
-                imMemento = loadImage ('./assets/memento.jpg ')
-                  imSchindler = loadImage ('./assets/schindler.jpg ')
-                    arrowHover = loadImage ('./assets/arrowhover.png ')
-                    arrow = loadImage ('./assets/arrow.png ')
-
-	// put preload code here
-}
-
-var movies = [];
-var poster = []
 
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
 
-  for(var i = 0; i < myData.films.length; i++ ){
+  createCanvas(windowWidth, windowHeight);
+  var nFigure = 200;
 
-    var movie = myData.films[i]; // la var è uguale al file peopl in space, di quello prendo l'array paople e dell'array prendo gli elementi
+  // create first object group
+  for (var i = 0; i < nFigure; i++) {
+       var myFigura = new figuraS(random(0, 50), random(500, height), 30);
+       noStroke();
+      figureS.push(myFigura);}
 
-    var x = 240+i*height/6 - 150;
-    var y = 590
-    var l = movie.title;
-
-    var newMovie = new Movie(x,y,100,l);
-    movies.push(newMovie);
-    newMovie.c = i
-  }
+// create second object group
+  for (var i = 0; i < nFigure; i++) {
+       var myFigura1 = new figuraS(random(width-50, width), random(0, height-500), 30);
+       noStroke();
+       figureD.push(myFigura1); myFigura1.color = 'blue'; myFigura1.shape = 1; myFigura1.position = 1;  }
 }
-
 
 function draw() {
-	background('#e2e0cf');
+  noStroke();
+  background(0);
+  fill('green');
   textAlign(CENTER);
   textSize(25);
-  if(frameCount < 300){
-      fill(0,0,0,200)
-      rect(width/2, height/2,width, height)
-      fill(255);
-      text('Drag the film\'\s titles into the corresponding film poster box.\nPut the mouse over the posters to get information about the plot of the corresponding film.', width/2, 450)
-  }
-  if( sposta == 1) {cursor(HAND);} else {
-    cursor(ARROW);
-  }
-    fill(0);
-    textSize(14);
- // pagina 1
-  if(button == 0 && mouseX > 95 && mouseX < 295 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[11]
-    text(testo1.storyline, width/2, 400)}
-  else if(button == 0 && mouseX > 325 && mouseX < 525 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[6]
-    text(testo1.storyline, width/2, 400)}
-  else if(button == 0 && mouseX > 555 && mouseX < 755 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[2]
-  text(testo1.storyline, width/2, 400)}
-  else if(button == 0 && mouseX > 785 && mouseX < 985 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[5]
-  text(testo1.storyline, width/2, 400)}
-  else if(button == 0 && mouseX > 1015 && mouseX < 1215 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[8]
-  text(testo1.storyline, width/2, 400)}
-  else if(button == 0 && mouseX > 1245 && mouseX < 1445 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[10]
-  text(testo1.storyline, width/2, 400)};
+  tempo = tempo - 1;
+  text('Time:',width/2-3, height/6 - 30);
+  text(tempo,width/2-3, height/6 +20);
+  text('Score:', width/2-3, 5*height/6 - 30);
+  text(t, width/2-3, 5*height/6+20);
+  fill(255);
 
-//pagina 2
+  if (frameCount < 400) {
+        text('Try to touch \n at least \n300 shapes to win',width/2-3, height/2-20);
+      }
+  if (tempo >= 0 && t >= 300) {
+        tempo = 1;
+        text('YOU WIN',width/2-3, height/2);
+      }
+  else if (tempo <= 0 && t < 300) {
+        tempo = 1;
+        text('GAME OVER',width/2-3, height/2);
+      };
 
+// draw the first object group
+  for (var j = 0; j < figureS.length; j++ ) {
+         figureS[j].count();
+         figureS[j].move();
+         figureS[j].change();
+         figureS[j].display();
+         if(tempo ==1 && t>=300 || tempo == 1 && t<=300) {
+           tempo =1;
+           figureS[j].speed = 0;
+         }}
 
-  if(button == 1 && mouseX > 95 && mouseX < 295 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[0]
-    text(testo1.storyline, width/2, 400)}
-  else if(button == 1 && mouseX > 325 && mouseX < 525 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[1]
-    text(testo1.storyline, width/2, 400)}
-  else if(button == 1 && mouseX > 555 && mouseX < 755 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[4]
-  text(testo1.storyline, width/2, 400)}
-  else if(button == 1 && mouseX > 785 && mouseX < 985 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[9]
-  text(testo1.storyline, width/2, 400)}
-  else if(button == 1 && mouseX > 1015 && mouseX < 1215 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[3]
-  text(testo1.storyline, width/2, 400)}
-  else if(button == 1 && mouseX > 1245 && mouseX < 1445 && mouseY > 50 && mouseY < 290) {
-    var testo1 = myData.films[7]
-  text(testo1.storyline, width/2, 400)}
+// draw the second object group
+  for (var j = 0; j < figureS.length; j++ ) {
+         figureD[j].count();
+         figureD[j].move();
+         figureD[j].change();
+         figureD[j].display();
+         if(tempo ==1 && t>=300 || tempo == 1 && t<=300) {
+           tempo =1;
+           figureD[j].speed = 0;
+         }}
 
-  textSize(12);
-  noFill()
+stroke(255);
+strokeWeight(20);
+line(2*width/5+6, 0, 2*width/5+6, frameCount*4);
+line(3*width/5-6, height-frameCount*4, 3*width/5-6, height);
 
-	for(var j = 0; j < movies.length; j++) {
-    movies[j].move();
-    movies[j].display();
 }
 
-  var eX = width/2 - 40;
-  var eY = 630;
+// create object
+function figuraS(_x, _y, _diameter) {
 
-  if (mouseX > eX && mouseX < eX + 64 && mouseY > eY && mouseY < eY + 67 && button == 0) {
-    click = 1} else {
-    click = 0}
+  this.x = _x;
+  this.y = _y;
+  this.diameter = _diameter;
+  this.speed = random(-1,5);
+  this.color =  'yellow';
+  this.shape = 0;
+  this.position = 0;
+  this.tocco = 1 ;
+  var yDir = 1;
+  var xDir = 1;
 
-   if(mouseX > eX && mouseX < eX + 64 && mouseY > eY && mouseY < eY + 67 && button == 0) {
-     image(arrowHover, eX, eY, 64, 67);
-     cursor(HAND);
-   } else if(cambiaPagina == 0){
-     image(arrow, eX, eY, 64, 67);
-   }
 
-  if( button == 0 ) {
-    rectMode(CORNER);
-    noFill()
-    rect (95, 290, 200, 40);
-    image(imMemento, 95,50, 200, 240);
-    //interstellar
-    rect (325, 290, 200, 40);
-    image(imInterstellar, 325.5,50, 200, 240);
-    //fight club
-    rect (555, 290, 200, 40);
-    image(imFightclub, 555.5,50, 200, 240);
-    //matrix
-    rect (785, 290, 200, 40);
-    image(imMatrix, 785.5,50, 200, 240);
-    //Psycho
-    rect (1015, 290, 200, 40);
-    image(imPsycho, 1015.5,50, 200, 240);
-    //the departed
-    rect (1245, 290, 200, 40);
-    image(imDeparted, 1245.5,50, 200, 240);
-    }
+// function to change object properties
+this.change =  function() {if(this.touch() && this.position == 0) {
+                              colorMode(HSB);
+                              this.color =  color(random(175, 299), 100, 100);
+                              this.shape = 1;
+                              this.tocco = 0;
+                          }
+                           else if (this.touch() && this.position == 1 ) {
+                              colorMode(HSB);
+                              this.color =  color(random(67, 0), 100, 100);
+                              this.shape = 0;
+                              this.tocco = 0;
+                          } }
 
- else if( button == 1) {
-    noFill()
-    rectMode(CORNER);
-    rect (95, 290, 200, 40);
-    image(imSchindler, 95,50, 200, 240);
-    //gladietor
-    rect (1245, 290, 200, 40);
-    image(imGladietor, 1245.5,50, 200, 240)
-    //forrestgump
-    rect (1015, 290, 200, 40);
-    image(imForrestgump, 1015.5,50, 200, 240);
-    //pianist
-    rect (785, 290, 200, 40);
-    image(imPianist, 785.5,50, 200, 240);
-    //inception
-    rect (555, 290, 200, 40);
-    image(imInception, 555.5,50, 200, 240);
-    //pulp fiction
-    rect (325, 290, 200, 40);
-    image(imPulpfiction, 325.5,50, 200, 240);}
-    rectMode(CENTER)
+// function to count how many time the mouse touch the shapes
+ this.count =  function() {if(this.touch() && this.tocco == 1) {
+                              console.log(this.tocco);
+                              this.tocco = 0;
+                               t = t+1
+                           }};
+
+
+
+
+this.display = function() {
+      fill(this.color);
+      if(this.shape == 0 ) {
+         ellipse (this.x, this.y, this.diameter);
+        //this.color = color(random(0, 255), random(0,25), random(0,25));
+      }
+      else if (this.shape == 1 ) {
+        rectMode(CENTER);
+        rect(this.x, this.y, 20, 20)}
+      }
+
+
+
+this.touch = function() {
+      var cambia =  ((this.x - mouseX) ** 2) + ((this.y - mouseY) ** 2) < 600;
+      return cambia;
+  };
+
+// function to move the object elements
+  this.move = function() {
+      this.x += this.speed * xDir;
+      this.y += this.speed * yDir;
+        if(this.y > height || this.y < 0) {yDir =yDir * -1;}
+
+        if(this.position == 0 && this.x > 2*width/5 || this.x < 0  ) {xDir =xDir * -1;}
+        else if ( this.position == 1 && this.x < 3*width/5 || this.x > width) {xDir =xDir * -1;}
+
 }
-
-function Movie(_x, _y, _diameter, _label) {
-	// Properties defined by constructor
-	this.size = _diameter;
-	this.x = _x;
-	this.y = _y;
-  this.testo = _label; // ettichetta per aggiungere il text ad ogni oggetto
-  this.c = 0;
-  this.color = '#e0dbaa';
-  this.match = 0;
-  noFill()
-	// Hardcoded properties
-	this.speed = 2;
-  this.touch = function() {
-        var cambia =  ((this.x - mouseX) ** 2) + ((this.y - mouseY) ** 2) < 1900;
-        return cambia;
-    };
-
-  this.move = function ()  {
-        if(cambiaPagina == 0) {
-        // memento rect (95, 290, 200, 40);
-        if (this.touch() && this.c ==  11 &&   this.x < 95 || this.x > 295 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 11 && this.x >95 && this.x < 295 && this.y > 290 && this.y < 330) {this.match = 1; };
-        // Interstellar rect (325, 290, 200, 40);
-        if (this.touch() && this.c ==  6 &&   this.x < 325 || this.x > 525 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 6 && this.x >325 && this.x < 525 && this.y > 290 && this.y < 330) {this.match = 1};
-        // fight club  rect (555, 290, 200, 40);
-        if (this.touch() && this.c ==  2 &&   this.x < 555 || this.x > 755 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 2 && this.x >555 && this.x < 755 && this.y > 290 && this.y < 330) {this.match = 1};
-        // matrix rect (785, 290, 200, 40);
-        if (this.touch() && this.c ==  5 &&   this.x < 785 || this.x > 985 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 5 && this.x >785 && this.x < 985 && this.y > 290 && this.y < 330) {this.match = 1};
-        // psyco rect (1015, 290, 200, 40);
-        if (this.touch() && this.c ==  8 &&   this.x < 1015 || this.x > 1215 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 8 && this.x >1015 && this.x < 1215 && this.y > 290 && this.y < 330) {this.match = 1};
-        //the departed rect (1245, 290, 200, 40);
-        if (this.touch() && this.c ==  10 &&   this.x < 1245 || this.x > 1445 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 10 && this.x >1245 && this.x < 1445 && this.y > 290 && this.y < 330) {this.match = 1};}
-        else if (cambiaPagina == 1) {this.x = _x; this.y = _y;  if(this.touch()) {cambiaPagina = 2}}
-
-   if(cambiaPagina == 2) {
-        //schindler
-        if (this.touch() && this.c ==  0 &&   this.x < 95 || this.x > 295 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 0 && this.x >95 && this.x < 295 && this.y > 290 && this.y < 330) {this.match = 1};
-        if (this.touch() && this.c ==  1 &&   this.x < 325 || this.x > 525 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 1 && this.x >325 && this.x < 525 && this.y > 290 && this.y < 330) {this.match = 1};
-        //inception
-        if (this.touch() && this.c ==  4 &&   this.x < 555 || this.x > 755 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 4 && this.x >555 && this.x < 755 && this.y > 290 && this.y < 330) {this.match = 1};
-        // pianist
-        if (this.touch() && this.c ==  9 &&   this.x < 785 || this.x > 985 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 9 && this.x >785 && this.x < 985 && this.y > 290 && this.y < 330) {this.match = 1};
-        // forrestgump
-        if (this.touch() && this.c ==  3 &&   this.x < 1015 || this.x > 1215 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 3 && this.x >1015 && this.x < 1215 && this.y > 290 && this.y < 330) {this.match = 1};
-        // gladietor
-        if (this.touch() && this.c ==  7 &&   this.x < 1245 || this.x > 1445 || this.y <290 || this.y >330) {this.match = 0} else if(this.c == 7 && this.x >1245 && this.x < 1445 && this.y > 290 && this.y < 330) {this.match = 1};
-   }
-  if(sposta == 1 && this.touch()) {this.x = mouseX; this.y = mouseY; fill(this.color)}
-  else if (sposta == 0 && this.match == 0) {this.x = _x; this.y = _y}
 }
-
-
-	this.display = function() {
-    if (this.touch()) {fill(this.color); cursor(HAND);};
-    rectMode(CENTER);
-		rect(this.x, this.y, this.size, this.size - 75);
-    fill('black');
-    textAlign(CENTER);
-    text(this.testo, this.x, this.y+4);
-    noFill();
-  	}
-    }
-
-function mouseDragged() {sposta = 1; }
-function mouseReleased() {sposta = 0; }
-function mousePressed() {if(click == 1){button = 1; cambiaPagina = 1} }
